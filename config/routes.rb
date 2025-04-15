a966b07209769278b4284faba979ada9ce322844
Sarah Goyette <sgoyette@conncoll.edu>
2025-04-14 23:53:41 -0400

    deleted not needed routes and controllers

--- config/routes.rb
+++ config/routes.rb
@@ -5,19 +5,5 @@ Rails.application.routes.draw do
   
   root "profile#home"
 
-  get "home/index"
-  get "home/about"
-  get "home/contactus"
-  get "home/help"
-  
-  resources :microposts do
-    member do
-      get :like
-    end
-  end
-
-  resources :users
-
-  #get 'microposts/:id/like' => 'microposts#like', as: :likeAPost
-  
+ 
 end
